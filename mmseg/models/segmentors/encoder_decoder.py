b7f6586e823ddbd32162ec7ca5dabe97e56cbf1d
Ubuntu <ubuntu@ip-172-31-3-50.us-east-2.compute.internal>
2021-09-29 20:45:31 +0000

    add support for segmentation map inference as opposed to argmax

--- mmseg/models/segmentors/encoder_decoder.py
+++ mmseg/models/segmentors/encoder_decoder.py
@@ -254,8 +254,7 @@ class EncoderDecoder(BaseSegmentor):
 
     def simple_test(self, img, img_meta, rescale=True):
         """Simple test with single image."""
-        seg_logit = self.inference(img, img_meta, rescale)
-        seg_pred = seg_logit.argmax(dim=1)
+        seg_pred = self.inference(img, img_meta, rescale)
         if torch.onnx.is_in_onnx_export():
             # our inference backend only support 4D output
             seg_pred = seg_pred.unsqueeze(0)
