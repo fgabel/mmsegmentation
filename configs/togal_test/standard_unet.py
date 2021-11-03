89804113ea6f9c80a461ed3553a0b945a76c10f7
Frank Gabel <frank240690@web.de>
2021-11-03 16:53:57 +0100

    put everything into one config that can be versionized

--- configs/togal_test/standard_unet.py
+++ configs/togal_test/standard_unet.py
@@ -1,7 +1,95 @@
-_base_ = [
-'../_base_/datasets/togal.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+wandb_project ='mmdetection'
+wandb_experiment_name = 'test1delete'
+
+######################################################################
+# optimizer
+optimizer = dict(type='Adam', lr=0.0001, weight_decay=0.00005)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(policy='poly', power=0.9, min_lr=3e-6, by_epoch=False)
+# runtime settings
+runner = dict(type='IterBasedRunner', max_iters=400000)
+checkpoint_config = dict(by_epoch=False, interval=20000)
+evaluation = dict(interval=30000, metric='mIoU', pre_eval=False)
+
+######################################################################
+# runtime settings
+log_config = dict(
+    interval=10,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook'),
+        dict(
+            type='WandbLoggerHook',
+            init_kwargs=dict(
+                project=wandb_project,
+                name=wandb_experiment_name))
+    ])
+
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None# 'work_dirs/standard_unet/latest.pth'
+workflow = [('train', 3), ('val', 1)]
+cudnn_benchmark = True
+
+######################################################################
+# dataset settings
+
+dataset_type = 'CustomDataset'
+
+data_root = 'data/'
+img_norm_cfg = dict(
+    mean=[127.5, 127.5, 127.5], std=[127.5, 127.5, 127.5], to_rgb=True)
+crop_size = (448, 448)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Normalize', **img_norm_cfg),
+    #dict(type='RGB2Gray')
+    dict(type='RandomRotate', prob=0.3, degree=180),
+    #dict(type='PhotoMetricDistortion',
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale = (448, 448),
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=8,
+    train=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='images/train',
+        ann_dir='annotations/train',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='images/val',
+        ann_dir='annotations/val',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='images/test',
+        ann_dir='annotations/test',
+        pipeline=test_pipeline))
+
+
 
 # model settings
 norm_cfg = dict(type='BN', requires_grad=True)
